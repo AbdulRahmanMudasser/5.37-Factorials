7ddfe3aa8b0901bb4ecd3698675199d9c3995e75
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 22:50:39 +0500

    solution done

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -2,7 +2,24 @@
 {
     static void Main()
     {
+        int number, factorial = 1;
 
+        Console.Write("Enter a nonnegative integer: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 0)
+        {
+            Console.WriteLine("Error: Negative integer entered.");
+        }
+        else
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+        }
     }
     // end of main method
 }
