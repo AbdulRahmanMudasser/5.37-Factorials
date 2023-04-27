65bb8934103352423321fc63887eac24a858ced7
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 22:51:37 +0500

    comments addes

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -2,15 +2,19 @@
 {
     static void Main()
     {
+        // declaring variables
         int number, factorial = 1;
 
-        Console.Write("Enter a nonnegative integer: ");
+        // taking input from user
+        Console.Write("Enter A Nonnegative Integer: ");
         number = Convert.ToInt32(Console.ReadLine());
 
+        // check if the number is negative
         if (number < 0)
         {
-            Console.WriteLine("Error: Negative integer entered.");
+            Console.WriteLine("Error: Negative Integer Entered.");
         }
+
         else
         {
             for (int i = 1; i <= number; i++)
